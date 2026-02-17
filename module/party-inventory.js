d74392d91379d0703817219beb4fc616a6de5f86
AniGam3r <74879877+AniGam3r@users.noreply.github.com>
2026-02-17 09:38:54 +0000

    Update import path for PartyInventory module

--- module/party-inventory.js
+++ module/party-inventory.js
@@ -1,9 +1,8 @@
 import { moduleId, localizationID } from './const.js';
-// Ensure these paths match where you save your files!
-import { PartyInventory } from './party-inventory.js'; 
+
+import { PartyInventory } from './apps/inventory.js'; 
 import { SplitCurrency } from "./apps/split-currency.js";
-import { TakeCurrency } from "./apps/take-currency.js";
-// You haven't sent sheet-inject.js yet, but we'll assume it's there
+
 import { addTogglePartyButton, addTogglePartyButtonTidy, addGroupInventoryIndicatorTidy } from './sheet-inject.js';
 
 Hooks.on('setup', () => {
